34c637be353fdd5bdd387a6f56a2d0221d49fbd0
kermakovich <kermakovich.laba@solvd.com>
2023-02-09 17:20:08 +0300

    Minor fixes were done

--- nodeBackReciever/src/app.ts
+++ nodeBackReciever/src/app.ts
@@ -2,11 +2,13 @@ import express from 'express';
 const app = express();
 const port = 3000;
 
+//http://localhost:3000/api/v1/customers
+
 app.use(express.json())
 
 app.post('/api/v1/customers', (req, res) => {
-  console.log(req.body);
-  res.send({'' :'record was added to database'});
+  console.log(req.body.data);
+  res.send({'message' :'record was added to database'});
 });
 
 app.listen(port, () => {
